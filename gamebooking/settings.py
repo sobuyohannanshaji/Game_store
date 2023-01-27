87f71632707712a48c8787e849ef46b48a24058b
sobu yohannan shaji <sobu003@gmail.com>
2023-01-27 16:52:09 +0530

    Commit

--- gamebooking/settings.py
+++ gamebooking/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s)!!*1x%dbd%sf(x2lu%2-y=1s#%3x*0j1dmx_aiz59kvnd*9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
